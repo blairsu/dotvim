78bbd159b95be80626ae4fb76f2d9f21f9401c10
Blair Su <blair@cameo.com.tw>
2013-12-05 14:36:14 +0800

    add alias.dt to view diff with vimdiff

--- git_setting.sh
+++ git_setting.sh
@@ -3,5 +3,6 @@ git config --global alias.st status
 git config --global alias.ci 'commit -v'
 git config --global alias.br branch
 git config --global alias.last 'log -1 HEAD'
-git config --global alias.logp 'log --graph --color --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
+git config --global alias.lp 'log --graph --color --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
 git config --global alias.unstage 'reset HEAD --'
+git config --global alias.dt 'difftool -t vimdiff -y'
